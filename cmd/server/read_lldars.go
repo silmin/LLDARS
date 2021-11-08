541696a497d7898bdeb169fb181b90c328ce7cd7
silmin <silvestmint@gmail.com>
2021-11-08 13:38:59 +0900

    header/payloadのRead処理を共通化

--- cmd/server/read_lldars.go
+++ cmd/server/read_lldars.go
@@ -7,33 +7,24 @@ import (
 )
 
 func ReadLLDARSHeader(conn net.Conn) lldars.LLDARSLayer {
-	var header []byte
-	for {
-		bufSize := lldars.LLDARSLayerSize - len(header)
-		if bufSize <= 0 {
-			break
-		}
-		buf := make([]byte, bufSize)
-		l, err := conn.Read(buf)
-		header = append(header, buf[:l]...)
-		Error(err)
-	}
-	rl := lldars.Unmarshal(header)
-
-	return rl
+	return lldars.Unmarshal(readConnData(conn, lldars.LLDARSLayerSize))
 }
 
 func ReadLLDARSPayload(conn net.Conn, length uint64) []byte {
-	var payload []byte
+	return readConnData(conn, int(length))
+}
+
+func readConnData(conn net.Conn, length int) []byte {
+	var data []byte
 	for {
-		bufSize := int(length) - len(payload)
+		bufSize := int(length) - len(data)
 		if bufSize <= 0 {
 			break
 		}
 		buf := make([]byte, bufSize)
 		l, err := conn.Read(buf)
-		payload = append(payload, buf[:l]...)
+		data = append(data, buf[:l]...)
 		Error(err)
 	}
-	return payload
+	return data
 }
